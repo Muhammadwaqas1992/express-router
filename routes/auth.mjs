bea784835765e4bc6815e3fa07dde89d98eae6d2
muhammad waqas <waqasrafi55@gmail.com>
2023-07-20 13:59:10 +0500

    new commit

---
+++ routes/auth.mjs
@@ -0,0 +1,123 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/login', (req, res, next) => {
+  res.send(`<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Routing Server</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- css -->
+        <link rel="stylesheet" href="./style.css" type="text/css">
+        <!-- font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+        <!-- icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <!-- moment js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+        <!-- javascript -->
+        <script src="./main.js"></script>
+    </head>
+    
+    <body>
+        <h1 class="center">Routing Server</h1>
+        <h3 class="center">This Is Routing Server</h3>
+        <h3 class="center" id="date"></h3>
+        <p class="center">Thanks For Visiting Regards! Muhammad Waqas&copy;</p>
+        <table>
+            <tr>
+                <td>1</td>
+                <td>Login V1</td>
+                <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/login"><i class="bi bi-arrow-up-right-square"></i></a></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Signup V1</td>
+                <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/signup"><i class="bi bi-arrow-up-right-square"></i></a></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Feed V1</td>
+                <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/feed/:userId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Post V1</td>
+                <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/post/:userId/:postId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Comment V1</td>
+                <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/comment/:postId/:commentId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+            </tr>
+        </table>
+    </body>
+    </html>`);
+});
+
+router.get('/signup', (req, res, next) => {
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Routing Server</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- css -->
+      <link rel="stylesheet" href="./style.css" type="text/css">
+      <!-- font -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+      <!-- icons -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+      <!-- moment js -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+      <!-- javascript -->
+      <script src="./main.js"></script>
+  </head>
+  
+  <body>
+      <h1 class="center">Routing Server</h1>
+      <h3 class="center">This Is Routing Server</h3>
+      <h3 class="center" id="date"></h3>
+      <p class="center">Thanks For Visiting Regards! Muhammad Waqas&copy;</p>
+      <table>
+          <tr>
+              <td>1</td>
+              <td>Login V1</td>
+              <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/login"><i class="bi bi-arrow-up-right-square"></i></a></td>
+          </tr>
+          <tr>
+              <td>2</td>
+              <td>Signup V1</td>
+              <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/signup"><i class="bi bi-arrow-up-right-square"></i></a></td>
+          </tr>
+          <tr>
+              <td>3</td>
+              <td>Feed V1</td>
+              <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/feed/:userId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+          </tr>
+          <tr>
+              <td>4</td>
+              <td>Post V1</td>
+              <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/post/:userId/:postId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+          </tr>
+          <tr>
+              <td>5</td>
+              <td>Comment V1</td>
+              <td><a href="https://wild-erin-bass-yoke.cyclic.app/api/v1/comment/:postId/:commentId"><i class="bi bi-arrow-up-right-square"></i></a></td>
+          </tr>
+      </table>
+  </body>
+  </html>`);
+
+})
+
+export default router;
---
+++ server.mjs
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+import authRouter from './routes/auth.mjs';
+import commentRouter from './routes/comment.mjs';
+import feedRouter from './routes/feed.mjs';
+import postRouter from './routes/post.mjs';
+
+const app = express();
+app.use(express.json()); // body parser
+app.use(cors());
+
+app.use(authRouter);
+app.use(commentRouter);
+app.use(feedRouter);
+app.use(postRouter);
+//     /static/vscode_windows.exe
+app.use("/static", express.static(path.join(__dirname, 'static')))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err); // Log the error for debugging purposes
+
+  // You can customize the error response based on the specific error properties
+  // or use a generic error message and status code.
+  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+});
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, () => {
+  console.log(`Example server listening on port ${PORT}`);
+});
